ae807c120124bf1bf6cb14dc00e961883991cb57
Folyd <lyshuhow@gmail.com>
2016-02-18 12:04:56 +0800

    Update:using vue.js,relaxing the content script policy,match file url

--- js/options.js
+++ js/options.js
@@ -1,22 +1,24 @@
 const storage = chrome.storage.local;
 
-$(function () {
-    storage.get(null, function (items) {
-        console.log('options:' + JSON.stringify(items));
-        const ul = $('#shortcut-list>ol');
-        const keys = Object.keys(items);
-        var key, url;
-        for (var i = 0; i < keys.length; i++) {
-            key = keys[i];
-            var tab = items[key];
-            url = tab.url;
-            console.log("key:" + key + " url:" + url);
-            var $favicon = $("<img src=" + tab.favicon + ">");
-            var $key = $("<span>" + key.toString() + "</span>").css("color", "#dd4814").css("font-size", "30px");
-            var $url = $("<a href=\"" + url.toString() + "\">" + url.toString() + "</a>").css("color", "blue");
-            var li = $("<li></li>").append($favicon).append($key).append("<span>  </span>").append($url);
-            li.css("background-color", i % 2 ? "#c4c6c2" : "white").css("margin-top", "10px");
-            ul.append(li);
+$(function() {
+    Vue.config.debug = true;
+    var vm = new Vue({
+        el: '#app',
+        data:{
+            items:[]
+        },
+        created: function() {
+            this.queryItems();
+        },
+        methods: {
+            queryItems: function() {
+                //Notice: this points to the vm instance
+                var self = this;
+                storage.get(null, function(items) {
+                    self.items = items;
+                    console.log('options:' + JSON.stringify(items));
+                });
+            }
         }
     });
 });
