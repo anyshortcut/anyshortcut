f24e0095cd937c012c3a428a68266fde40e38365
Folyd <lyshuhow@gmail.com>
2016-02-21 01:24:46 +0800

    Update:integrate google material design lite framework,make options.html open in new tab

--- js/inject-unbound-tips.js
+++ js/inject-unbound-tips.js
@@ -1,5 +1,5 @@
-var el = '<div id="inject-tips">'
-+'<h3>the shortcut key not bound any url yet!</h3>'
-+'<button id="got-it-button">Got it</button>'
-+'</div>';
-document.body.insertAdjacentHTML('beforeEnd',el);
+var el = `<div id="inject-tips">
+  <h3>the shortcut key not bound any url yet!</h3>
+  <button id="got-it-button">Got it</button>
+  </div>';`
+document.body.insertAdjacentHTML('beforeEnd', el);
