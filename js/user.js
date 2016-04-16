db3be7e16e9fa240a5a014e73ff51e8c28e5d5db
Folyd <lyshuhow@gmail.com>
2016-04-16 12:41:09 +0800

    Update:add sign in feature

--- js/user.js
+++ js/user.js
@@ -6,25 +6,46 @@ window.onload = function() {
     var vm = new Vue({
             el: 'body',
             data: {
+                registered: true,
                 username: '',
                 password: ''
             },
+            computed: {
+                authData: function() {
+                    //Maybe MD5 encrypt the user password?
+                    return {email: this.username, password: this.password};
+                }
+            },
             methods: {
-                register: function() {
+                signUp: function() {
                     if (this.username === '' || this.password === '') {
                         alert("empty field");
                     }
 
-                    firebase.createUser({email: this.username, password: this.password},
+                    firebase.createUser(this.authData,
                         (error, userData) => {
                             if (error) {
                                 console.log("Error creating user:", error);
+                                this.registered = false;
                             } else {
                                 console.log("Successfully created user account with uid:", userData.uid);
+                                this.registered = true;
+                                this.username = this.password = '';
                             }
-
-                            this.username = this.password = '';
                         });
+                },
+                signIn: function() {
+                    firebase.authWithPassword(this.authData,
+                        (error, authData) => {
+                            if (error) {
+                                console.log("Login Failed!", error);
+                            } else {
+                                console.log("Authenticated successfully with payload:", authData);
+                            }
+                        });
+                },
+                switchToSingUpState: function() {
+                    this.registered = false;
                 }
             }
         }
