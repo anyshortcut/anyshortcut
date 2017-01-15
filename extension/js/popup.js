a0faa4d58a8341e67f06f9a992c68ee5d788cbd4
Folyd <lyshuhow@gmail.com>
2017-01-15 12:31:45 +0800

    Update: integrate OriginBound code to popup.js directly

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
-import OriginBound from "../component/OriginBound.vue";
-import OptionBound from "../component/OptionBound.vue";
+import moment from 'moment';
+import Keyboard from '../component/Keyboard.vue';
 import common from "./common.js";
 import auth from "./background/auth.js";
 
@@ -10,7 +10,14 @@ window.onload = function() {
     let vm = new Vue({
         el: 'body',
         data: {
-            tab: {}
+            tab: {},
+            bound: false, // A flag indicate origin bound.
+            key: '', // Shortcut key for origin bound.
+            value: {}, // Origin bound value.
+            boundTips: '',// Origin bound tips.
+            boundKeys: null,// All bound keys, for keyboard component usage.
+            comment: null,
+            primary: true,
         },
         computed: {
             authenticated: function() {
@@ -18,17 +25,53 @@ window.onload = function() {
             }
         },
         components: {
-            OriginBound,
-            OptionBound
+            Keyboard
+        },
+        filters: {
+            //Custom filter to use moment.js format time as fromNow type.
+            fromNow: function(time) {
+                return moment(time).fromNow();
+            }
         },
         methods: {
             loginWithGoogle: function() {
                 auth.openAuthPopupWindow();
+            },
+            handleShortcutBinding: function() {
+                chrome.runtime.sendMessage({save: true, key: this.key, comment: this.comment}, response => {
+                    this.bound = true;
+                    this.boundTips = 'Great job!you have bound a shortcut for this url!';
+                });
+            },
+            handleShortcutUnbinding: function() {
+                chrome.runtime.sendMessage({remove: true, key: this.key}, result => {
+                    if (result) {
+                        this.bound = false;
+                        this.key = '';
+                        this.value = {};
+
+                        this.boundTips = 'Delete Success!';
+                    }
+                });
             }
         },
         created: function() {
             common.getCurrentTab(tab => {
                 this.tab = tab;
+                // Request check current tab url was bound in message-handler.js
+                chrome.runtime.sendMessage({check: true}, bindInfo => {
+                    // bind info. {"key":key,"value":value}
+                    //TODO How to check javascript object null or undefined properly?
+                    if (bindInfo) {
+                        this.bound = true;
+                        this.key = bindInfo.key;
+                        this.value = bindInfo.value;
+                    }
+                });
+            });
+
+            chrome.runtime.sendMessage({keys: true}, keys => {
+                this.boundKeys = keys ? keys : null;
             });
         }
     });
