7923ae35edc50dd3638c87904ea42cf4cd0834c8
Folyd <lyshuhow@gmail.com>
2017-05-03 23:27:26 +0800

    Update: add url-loader to handle image files

--- webpack.config.js
+++ webpack.config.js
@@ -51,6 +51,14 @@ module.exports = {
                         }
                     }],
                 })
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    // name: utils.assetsPath('img/[name].[hash:7].[ext]')
+                }
             }
         ],
     },
