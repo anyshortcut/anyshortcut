3a11fa439cf39c11abf0d754ee8578b3c22f0ab5
Folyd <lyshuhow@gmail.com>
2017-05-17 00:57:28 +0800

    Update: update Welcome.vue ui and copywriting

--- js/background/auth.js
+++ js/background/auth.js
@@ -1,11 +1,4 @@
-import config from "../config.js";
-import injector from "./injector.js";
-
 let auth = {
-    openAuthPage(){
-        let url = config.baseURL + '/app.html#/login';
-        window.open(url);
-    },
     isAuthenticated(){
         return !!localStorage.getItem('token');
     },
--- view/Welcome.vue
+++ view/Welcome.vue
@@ -1,22 +1,44 @@
 <template>
-    <div>
-        <div>
-            <a href="https://anyshortcut.com" target="_blank">
-                <span style="font-weight: bold">anyshortcut</span></a>
+    <div class="content">
+        <div class="logo">
+            <img src="../extension/images/icon48.png" alt=""><span>anyshortcut</span>
         </div>
-        <button @click="loginWithGoogle">Log in</button>
+        <a :href="signInUrl" target="_blank">Sign in</a> now to boost your productivity!
+        <br>
+        <b>It's free!</b>
     </div>
 </template>
 <style lang="css">
+    .content {
+        line-height: 2em;
+    }
+
+    .logo {
+        color: #4F6EC8;
+        font-size: 35px;
+        font-weight: bold;
+        font-family: "Arial Rounded MT Bold", serif;
+        margin: 30px;
+    }
+
+    .logo img {
+        vertical-align: middle;
+        padding: 10px;
+    }
+
+    a:focus {
+        text-decoration: none;
+        outline: none;
+    }
 </style>
 <script type="es6">
-    import auth from "../js/background/auth.js";
+    import config from "../js/config.js";
 
     export default{
         name: 'welcome-view',
-        methods: {
-            loginWithGoogle: function() {
-                auth.openAuthPage();
+        computed: {
+            signInUrl: function() {
+                return config.baseURL + '/sign_in';
             }
         }
     }
