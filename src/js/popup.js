7271e362238baefdbc1f50e45b94486bfe935547
Folyd <lyshuhow@gmail.com>
2017-05-22 00:40:24 +0800

    Update: add message.js to handle Message component logic

--- src/js/popup.js
+++ src/js/popup.js
@@ -3,64 +3,9 @@ import auth from "./background/auth.js";
 import WelcomeView from "../view/Welcome.vue";
 import MainView from "../view/Main.vue";
 import PreferenceView from "../view/Preferences.vue";
+import Message from "../component/message.js";
 
 require("../less/popup.less");
-
-let MessageConstructor = Vue.extend(require("../component/Message.vue"));
-
-let instances = [];
-let seed = 1;
-
-let Message = function(options) {
-    options = options || {};
-    if (typeof options === 'string') {
-        options = {
-            message: options
-        };
-    }
-    let userOnClose = options.onClose;
-    let id = 'message_' + seed++;
-
-    options.onClose = function() {
-        Message.close(id, userOnClose);
-    };
-
-    let instance = new MessageConstructor({
-        data: options
-    });
-    instance.id = id;
-    instance.vm = instance.$mount();
-    document.body.appendChild(instance.vm.$el);
-    instance.vm.visible = true;
-    instance.dom = instance.vm.$el;
-    instance.dom.style.zIndex = 10000;
-    instances.push(instance);
-    return instance.vm;
-};
-
-['success', 'warning', 'info', 'error'].forEach(type => {
-    Message[type] = options => {
-        if (typeof options === 'string') {
-            options = {
-                message: options
-            };
-        }
-        options.type = type;
-        return Message(options);
-    };
-});
-
-Message.close = function(id, userOnClose) {
-    for (let i = 0, len = instances.length; i < len; i++) {
-        if (id === instances[i].id) {
-            if (typeof userOnClose === 'function') {
-                userOnClose(instances[i]);
-            }
-            instances.splice(i, 1);
-            break;
-        }
-    }
-};
 
 Vue.prototype.$message = Message;
 
