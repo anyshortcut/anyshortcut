280eb08642414b4bdb0f86d494ea8201f69aac14
Folyd <lyshuhow@gmail.com>
2017-05-26 17:25:24 +0800

    Update: remove bind shortcut in open modal feature

--- src/js/script/modal.js
+++ src/js/script/modal.js
@@ -12,12 +12,9 @@ function removeElementDelay(element, delay) {
 }
 
 /**
- *
  * @param message the message to show in modal, text or html
- * @param positiveCallback
- * @param showNegativeButton whether to show the negative button.
  */
-function openModal(message, positiveCallback = null, showNegativeButton = true) {
+function openModal(message) {
     let modal = document.createElement('div');
     modal.className = 'modal-overlay';
 
@@ -26,27 +23,13 @@ function openModal(message, positiveCallback = null, showNegativeButton = true) 
     div.innerHTML = message;
     div.firstChild.className = 'as-injection-p';
 
-    let positiveButton = document.createElement('button');
-    positiveButton.textContent = 'Yes';
-    positiveButton.className = 'as-injection-button';
-    positiveButton.onclick = () => {
+    let confirmButton = document.createElement('button');
+    confirmButton.textContent = 'Yes';
+    confirmButton.className = 'as-injection-button';
+    confirmButton.onclick = () => {
         removeElementDelay(modal, 50);
-        if (positiveCallback) {
-            positiveCallback();
-        }
     };
-    div.appendChild(positiveButton);
-
-    if (showNegativeButton) {
-        let negativeButton = document.createElement('button');
-        negativeButton.textContent = 'No';
-        negativeButton.className = 'as-injection-button';
-        negativeButton.onclick = () => {
-            removeElementDelay(modal, 50);
-        };
-        div.appendChild(negativeButton);
-    }
-
+    div.appendChild(confirmButton);
 
     let timeoutId = removeElementDelay(modal);
     div.onmouseover = function(e) {
@@ -72,48 +55,24 @@ function openModal(message, positiveCallback = null, showNegativeButton = true) 
     document.body.insertAdjacentElement('beforeEnd', modal);
 }
 
-function showBoundSuccessTipDialog() {
-    let divTips = document.createElement('div');
-    divTips.className = 'as-injection-div';
-    divTips.innerHTML = 'Great! you bound the url with this key!';
-    document.body.insertAdjacentElement('beforeEnd', divTips);
-
-    removeElementDelay(divTips)
-}
-
 export default {
     showPrimaryShortcutUnbound(pressedKey){
         let innerHtml = `<p>the shortcut key <span id="as-injection-shortcut">ALT+SHIFT+${pressedKey}</span> 
-            not bound for this domain yet!</p>
-            <p>Would you like to bound this primary key with the url?</p>`;
-        openModal(innerHtml, function() {
-            chrome.runtime.sendMessage({save: true, key: pressedKey}, response => {
-                showBoundSuccessTipDialog();
-            });
-        });
+            not bound for this domain yet!</p>`;
+        openModal(innerHtml);
     },
     showSecondaryShortcutUnbound(pressedKey){
         let innerHtml = `<p>the secondary shortcut key <span id="as-injection-shortcut">ALT+${pressedKey}</span> 
-                        not bound for this domain yet!</p><p>Would you like to bound this secondary key with the domain?</p>`;
-        openModal(innerHtml, function() {
-            chrome.runtime.sendMessage({
-                secondarySave: true,
-                key: pressedKey,
-                comment: null,
-            }, response => {
-                showBoundSuccessTipDialog();
-            });
-        });
+                        not bound for this domain yet!</p>`;
+        openModal(innerHtml);
     },
     showQuickSecondaryShortcutFailed(primaryPressedKey, secondaryPressedKey){
         let innerHtml = `<p>the quick secondary shortcut key 
                         <span id="as-injection-shortcut">SHIFT+ALT+${primaryPressedKey}➯${secondaryPressedKey}</span> 
                         not bound yet!</p>`;
-        openModal(innerHtml, null, false);
+        openModal(innerHtml);
     },
     showShortcutKeyboard(){
-        let src = location.protocol + '//anyshortcut.com';
-        let frame = `<iframe src="${src}"></iframe>`;
-        openModal(frame, null, null);
+        // Do nothing yet
     }
 }
