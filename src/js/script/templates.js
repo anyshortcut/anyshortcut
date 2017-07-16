d95b31cf9cfb8ce157a3721e0331c4ade8d12681
Folyd <lyshuhow@gmail.com>
2017-07-16 23:58:28 +0800

    Update: template add more class style

--- src/js/script/templates.js
+++ src/js/script/templates.js
@@ -5,26 +5,32 @@ const shortcutNotFound = '<p>The <%- shortcutType %> shortcut key <span class="a
 
 const queryShortcutFailed = '<p>Neither <span class="anyshortcut-shortcut">SHIFT+ALT+<%- firstKey %><%- secondKey %></span> nor <span class="anyshortcut-shortcut">SHIFT+ALT+<%- firstKey %>➯<%- secondKey %></span> bound yet!</p>';
 
-const queryShortcutChooser = `<ul>
-                            <% _.forEach(shortcuts, function(shortcut, key) { %>
-                            <li><div>
-                            <%- key + 1 %> <img class="shortcut-favicon" src="<%- shortcut.favicon %>" alt="favicon"> <%- shortcut.comment %>
-                            </div></li>
-                            <% }); %> 
-                        </ul>`;
+const queryShortcutChooser = `
+    <ul>
+        <% _.forEach(shortcuts, function(shortcut, key) { %>
+        <li class="anyshortcut-shortcut-item">
+            <span class="anyshortcut-secondary-shortcut" title="<%- shortcut.title %>"><%- key + 1 %></span>
+            <img class="shortcut-favicon" src="<%- shortcut.favicon %>" alt="favicon">
+            <a href="<%- shortcut.url %>" title="<%- shortcut.url %>" target="_blank"><%- shortcut.comment %></a>
+        </li>
+        <% }); %>
+    </ul>`;
 
-const shortcutList = `<div>Secondary shortcuts for <span class="anyshortcut-shortcut">ALT+SHIFT+<%- key %></span><ul>
-                            <% _.forOwn(shortcuts, function(shortcut) { %>
-                            <li><div>
-                            <%- shortcut.key %> <img class="shortcut-favicon" src="<%- shortcut.favicon %>" alt="favicon"> <%- shortcut.comment %>
-                            </div></li>
-                            <% }); %> 
-                      </ul></div>`;
+const shortcutList = `<div>Secondary shortcuts for <span class="anyshortcut-shortcut">ALT+SHIFT+<%- key %></span>
+    <ul>
+        <% _.forOwn(shortcuts, function(shortcut) { %>
+        <li class="anyshortcut-shortcut-item">
+            <span class="anyshortcut-secondary-shortcut" title="<%- shortcut.title %>"><%- shortcut.key %></span>
+            <a href="<%- shortcut.url %>" title="<%- shortcut.url %>" target="_blank"><%- shortcut.comment %></a>
+        </li>
+        <% }); %>
+    </ul>
+</div>`;
 
 const shortcutListEmpty = `<img class="grey-balloons" alt="">
-                         <div>No secondary shortcuts for 
-                             <span class="anyshortcut-shortcut">ALT+SHIFT+<%- key %></span>
-                         </div>`;
+<div>No secondary shortcuts for
+    <span class="anyshortcut-shortcut">ALT+SHIFT+<%- key %></span>
+</div>`;
 
 export default {
     shortcutNotFound,
