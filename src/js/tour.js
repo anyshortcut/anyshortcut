e90a44e1a218a7f795e9549cdad340cb27540e86
Folyd <lyshuhow@gmail.com>
2017-08-05 22:54:38 +0800

    Update: tour.js add primary and secondary shortcut detect feature

--- src/js/tour.js
+++ src/js/tour.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import auth from "./background/auth.js";
 import config from "../js/config.js";
+import helper from './script/helper.js';
 
 const app = new Vue({
     el: "#app",
@@ -17,6 +18,10 @@ const app = new Vue({
                 "height": 800
             });
         },
+        openShortcut(url) {
+            window.open(url);
+            this.currentStep += 1;
+        },
     },
     created() {
         this.currentStep = auth.isAuthenticated() ? 1 : 0;
@@ -29,3 +34,104 @@ chrome.runtime.onMessageExternal.addListener(function(message, sender, sendRespo
     app.currentStep = 1;
 });
 
+const EMPTY_KEY = {
+    keyCode: 0,
+    keyCodeChar: null,
+    altKey: false,
+    shiftKey: false,
+    ctrlKey: false,
+    metaKey: false,
+    pressedAt: null,
+    releasedAt: null,
+};
+
+let firstKey = EMPTY_KEY;
+let secondKey = EMPTY_KEY;
+let triggerTimeoutId = null;
+
+
+function monitorKeyUp(e) {
+    if (!helper.isValidFullModifier(e)) {
+        // Ignore invalid modifier key
+        return;
+    }
+
+    if (helper.isValidKeyCode(e.keyCode)) {
+        if (!firstKey.releasedAt) {
+            firstKey.releasedAt = Date.now();
+        } else if (!secondKey.releasedAt) {
+            secondKey.releasedAt = Date.now();
+        }
+
+        triggerShortcut()
+    }
+}
+
+function monitorKeyDown(e) {
+    if (!helper.isValidFullModifier(e)) {
+        // Ignore invalid full modifier key
+        return;
+    }
+
+    let keyCode = e.keyCode;
+    if (!helper.isValidKeyCode(keyCode)) {
+        // Ignore invalid key code.
+        return;
+    }
+
+    // Prevent repeat trigger down event.
+    if (e.repeat) {
+        return;
+    }
+
+    let pressedKey = {
+        keyCode: keyCode,
+        keyCodeChar: String.fromCharCode(keyCode),
+        shiftKey: e.shiftKey,
+        altKey: e.altKey,
+        ctrlKey: e.ctrlKey,
+        metaKey: e.metaKey,
+        pressedAt: Date.now(),
+        releasedAt: null,
+    };
+
+    if (!firstKey.pressedAt) {
+        firstKey = pressedKey;
+    } else if (!secondKey.pressedAt) {
+        secondKey = pressedKey;
+    }
+}
+
+function triggerShortcut() {
+    if (triggerTimeoutId) {
+        // Clear previous session timeout if existed
+        window.clearTimeout(triggerTimeoutId);
+        triggerTimeoutId = null;
+    }
+
+    if (firstKey.pressedAt && firstKey.releasedAt) {
+        if (helper.isValidFullModifier(firstKey)) {
+            if (secondKey.pressedAt && secondKey.releasedAt) {
+                if (app.currentStep === 2 && firstKey.keyCodeChar === 'G' && secondKey.keyCodeChar === 'I') {
+                    app.openShortcut('https://inbox.google.com');
+                }
+                cleanUp();
+            } else {
+                triggerTimeoutId = window.setTimeout(function() {
+                    if (app.currentStep === 1 && firstKey.keyCodeChar === 'G') {
+                        app.openShortcut('https://www.google.com');
+                    }
+                    cleanUp();
+                }, helper.delayTime);
+            }
+        }
+    }
+}
+
+function cleanUp() {
+    firstKey = EMPTY_KEY;
+    secondKey = EMPTY_KEY;
+}
+
+document.addEventListener('keyup', monitorKeyUp, false);
+document.addEventListener('keydown', monitorKeyDown, false);
