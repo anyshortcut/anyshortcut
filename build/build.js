ab0b927b4ebbd9b01a4cc3d8cc5abe1b98990b04
Folyd <lyshuhow@gmail.com>
2017-11-10 15:19:58 +0800

    Generate different manifest.json for different environment

--- build/build.js
+++ build/build.js
@@ -5,6 +5,8 @@ const ora = require('ora');
 const chalk = require('chalk');
 const webpack = require('webpack');
 const webpackConfig = require('./webpack.config');
+const config = require('./config');
+const fs = require('fs');
 
 const spinner = ora('building for production...');
 spinner.start();
@@ -13,13 +15,27 @@ webpack(webpackConfig, function(err, stats) {
     spinner.stop();
     if (err) throw err;
     process.stdout.write(stats.toString({
-            colors: true,
-            modules: false,
-            children: false,
-            chunks: false,
-            chunkModules: false
-        }) + '\n\n');
+        colors: true,
+        modules: false,
+        children: false,
+        chunks: false,
+        chunkModules: false
+    }) + '\n\n');
 
     utils.execute("terminal-notifier -title 'Spruce' -message 'build complete' -sound default");
     console.log(chalk.cyan('  Build complete.\n'));
+});
+
+// Generate manifest.json for different environment
+fs.readFile('./build/manifest.json', (error, data) => {
+    if (error) console.log(error);
+    const manifest = JSON.parse(data);
+
+    manifest['name'] = config.env.name;
+    manifest['key'] = config.env.manifestKey;
+
+    fs.writeFile('./extension/manifest.json', JSON.stringify(manifest), error => {
+        if (error) console.log(error);
+    })
+
 });
--- build/config.js
+++ build/config.js
@@ -6,16 +6,22 @@ const env = {
         debug: false,
         schema: 'https://',
         domain: 'anyshortcut.com',
+        name: 'Anyshortcut',
+        manifestKey: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtsU34L7ro2a1etZPGPg4rRD5gVP4I7Px1mZuJqafP5Bqoh+wtVmOZgGNS3nlyGBfLCYphC3mmtYrZ8OvXLb+fRIe0sk/k/F+cIEZPmExin0epDN4jtA8ptT+FMDf6bFxRfwMrJCHMpjsfNWqhfrVEIBsOHQiDGQyy/05fYfwWl/XocFfEmKWayYiwdijSLa5io/dS71qp0VeDwudbQo9El9cf0CZdbTbYziVORXf7BQQNW56o/51Tg4X6kdNqs0Ck9c1rnZIqf92sNRmsfHKdhBFpWN1d3hD9rJ7ROriq69f3HjbOX3pAAJHJX7JqwapvGbk7A1loyAwbDgo8VQICwIDAQAB',
     },
     stage: {
         debug: false,
         schema: 'https://',
         domain: 'stage.anyshortcut.com',
+        name: 'Anyshortcut-Stage',
+        manifestKey: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtsU34L7ro2a1etZPGPg4rRD5gVP4I7Px1mZuJqafP5Bqoh+wtVmOZgGNS3nlyGBfLCYphC3mmtYrZ8OvXLb+fRIe0sk/k/F+cIEZPmExin0epDN4jtA8ptT+FMDf6bFxRfwMrJCHMpjsfNWqhfrVEIBsOHQiDGQyy/05fYfwWl/XocFfEmKWayYiwdijSLa5io/dS71qp0VeDwudbQo9El9cf0CZdbTbYziVORXf7BQQNW56o/51Tg4X6kdNqs0Ck9c1rnZIqf92sNRmsfHKdhBFpWN1d3hD9rJ7ROriq69f3HjbOX3pAAJHJX7JqwapvGbk7A1loyAwbDgo8VQICwIDAQAB',
     },
     development: {
         debug: true,
         schema: 'http://',
         domain: 'dev.anyshortcut.com',
+        name: 'Anyshortcut-Dev',
+        manifestKey: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtsU34L7ro2a1etZPGPg4rRD5gVP4I7Px1mZuJqafP5Bqoh+wtVmOZgGNS3nlyGBfLCYphC3mmtYrZ8OvXLb+fRIe0sk/k/F+cIEZPmExin0epDN4jtA8ptT+FMDf6bFxRfwMrJCHMpjsfNWqhfrVEIBsOHQiDGQyy/05fYfwWl/XocFfEmKWayYiwdijSLa5io/dS71qp0VeDwudbQo9El9cf0CZdbTbYziVORXf7BQQNW56o/51Tg4X6kdNqs0Ck9c1rnZIqf92sNRmsfHKdhBFpWN1d3hD9rJ7ROriq69f3HjbOX3pAAJHJX7JqwapvGbk7A1loyAwbDgo8VQICwIDAQAB',
     }
 };
 
