a6201fc150c26b1d945fe4eaa077af2d720f291a
Folyd <lyshuhow@gmail.com>
2017-12-12 20:29:01 +0800

    Bugfix: fix circle popup key trigger inpropperly bug

--- src/js/script/circle.js
+++ src/js/script/circle.js
@@ -3,6 +3,10 @@ import helper from "./helper.js";
 
 let popup = null;
 let circle = null;
+/**
+ * A flag indicates whether the key was pressed.
+ */
+let keyPressed = false;
 
 function buildCircle() {
     let circle = utils.createDiv('anyshortcut-circle');
@@ -55,6 +59,28 @@ export default {
         }, response => {
             let shortcuts = response.shortcuts;
 
+            document.addEventListener('keydown', e => {
+                if (helper.isActiveElementEditable()) {
+                    return;
+                }
+                if (helper.isValidKeyCode(e.keyCode) && helper.withoutAnyModifier(e)) {
+                    let keyCodeChar = String.fromCharCode(e.keyCode);
+
+                    if (window.isQueryShortcutChooserShowing && ['1', '2'].includes(keyCodeChar)) {
+                        return;
+                    }
+
+                    // Prevent repeat
+                    if (e.repeat) {
+                        return;
+                    }
+
+                    if (shortcuts.hasOwnProperty(keyCodeChar)) {
+                        keyPressed = true;
+                    }
+                }
+            });
+
             document.addEventListener('keyup', e => {
                 if (helper.isActiveElementEditable()) {
                     return;
@@ -67,8 +93,9 @@ export default {
                         return;
                     }
 
-                    if (shortcuts.hasOwnProperty(keyCodeChar)) {
+                    if (shortcuts.hasOwnProperty(keyCodeChar) && keyPressed) {
                         helper.openShortcut(shortcuts[keyCodeChar], false);
+                        keyPressed = false;
                     }
                 }
             });
