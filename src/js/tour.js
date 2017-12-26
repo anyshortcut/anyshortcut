bededdaf3d3fddc933ffff9d9313232abf01052e
Folyd <lyshuhow@gmail.com>
2017-12-26 21:23:18 +0800

    Update: add key event monitor after user login in tour page

--- src/js/tour.js
+++ src/js/tour.js
@@ -8,13 +8,14 @@ import common from "./common.js";
 import client from "./client.js";
 import config from "./config.js";
 import ga from "./mixin-ga.js";
-import helper from './script/helper.js';
 import Raven from "raven-js";
 import RavenVue from 'raven-js/plugins/vue';
+import monitor from './script/key-event-monitor.js';
 
 Raven.config('https://0aa6274679824a129c33c2cc4ae0d22b@sentry.io/144189').addPlugin(RavenVue, Vue).install();
 
 let $background = chrome.extension.getBackgroundPage();
+window.delay = true;
 
 const app = new Vue({
     el: "#app",
@@ -65,6 +66,11 @@ const app = new Vue({
             client.getDefaultShortcuts().then(data => {
                 this.defaultShortcuts = data;
             });
+
+            if (this.currentStep === 2) {
+                document.addEventListener('keyup', monitor.onKeyUp, false);
+                document.addEventListener('keydown', monitor.onKeyDown, false);
+            }
         }
     },
     mounted() {
@@ -74,96 +80,6 @@ const app = new Vue({
     },
     mixins: [ga],
 });
-
-
-const EMPTY_KEY = {
-    keyCode: 0,
-    keyCodeChar: null,
-    altKey: false,
-    shiftKey: false,
-    ctrlKey: false,
-    metaKey: false,
-    pressedAt: null,
-    releasedAt: null,
-};
-
-let firstKey = EMPTY_KEY;
-let secondKey = EMPTY_KEY;
-let triggerTimeoutId = null;
-
-
-function monitorKeyUp(e) {
-    if (helper.withAltModifier(e) && helper.isValidKeyCode(e.keyCode)) {
-        if (!firstKey.releasedAt) {
-            firstKey.releasedAt = Date.now();
-        } else if (!secondKey.releasedAt) {
-            secondKey.releasedAt = Date.now();
-        }
-
-        triggerShortcut()
-    }
-}
-
-function monitorKeyDown(e) {
-    if (helper.withAltModifier(e) && helper.isValidKeyCode(e.keyCode)) {
-        // Prevent repeat trigger down event.
-        if (e.repeat) {
-            return;
-        }
-
-        let keyCode = e.keyCode;
-        let pressedKey = {
-            keyCode: keyCode,
-            keyCodeChar: String.fromCharCode(keyCode),
-            shiftKey: e.shiftKey,
-            altKey: e.altKey,
-            ctrlKey: e.ctrlKey,
-            metaKey: e.metaKey,
-            pressedAt: Date.now(),
-            releasedAt: null,
-        };
-
-        if (!firstKey.pressedAt) {
-            firstKey = pressedKey;
-        } else if (!secondKey.pressedAt) {
-            secondKey = pressedKey;
-        }
-    }
-}
-
-function triggerShortcut() {
-    if (triggerTimeoutId) {
-        // Clear previous session timeout if existed
-        window.clearTimeout(triggerTimeoutId);
-        triggerTimeoutId = null;
-    }
-
-    if (firstKey.pressedAt && firstKey.releasedAt) {
-        if (helper.withAltModifier(firstKey)) {
-            if (secondKey.pressedAt && secondKey.releasedAt) {
-                if (app.currentStep === 3 && firstKey.keyCodeChar === 'G' && secondKey.keyCodeChar === 'I') {
-                    app.openShortcut('https://inbox.google.com');
-                }
-                cleanUp();
-            } else {
-                triggerTimeoutId = window.setTimeout(function() {
-                    if (app.currentStep === 2 && firstKey.keyCodeChar === 'G') {
-                        app.openShortcut('https://www.google.com');
-                    }
-                    cleanUp();
-                }, helper.delayTime);
-            }
-        }
-    }
-}
-
-function cleanUp() {
-    firstKey = EMPTY_KEY;
-    secondKey = EMPTY_KEY;
-}
-
-document.addEventListener('keyup', monitorKeyUp, false);
-document.addEventListener('keydown', monitorKeyDown, false);
 
 window.addEventListener('storage', event => {
     // A storage event fired because of localStorage value changed.
